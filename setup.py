d8bea1a600a993ab20afe25d6f1c647286b9d574
abdullahal-baki <abdullahalbaki009@gmail.com>
2025-05-26 13:04:09 +0600

    readme updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='sample_package_abdullah',
-    version='0.1.2',
+    version='0.1.3',
     author='Abdullah Al Baki',
     author_email='abdullahalbaki009@gmail.com',
     description='A sample Python package',
