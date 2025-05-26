279d6a8b6ea6ad104f82a2b44e27c293664f177a
abdullahal-baki <abdullahalbaki009@gmail.com>
2025-05-26 13:36:55 +0600

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,23 +5,25 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='sample_package_abdullah',
-    version='0.1.4',
+    version='0.1.5',
     author='Abdullah Al Baki',
     author_email='abdullahalbaki009@gmail.com',
     description='A sample Python package',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/abdullahal-baki/sample_package',  #home page
+    license='MIT', 
     packages=find_packages(include=['sample_package', 'sample_package.*']),
     install_requires=[
         'requests>=2.25.1',
     ],
-    
     project_urls={
-        "Source": "https://github.com/psf/requests"
+        "Source": "https://github.com/abdullahal-baki/sample_package",
+        "Documentation": "https://github.com/abdullahal-baki/sample_package#readme",
+        "Bug Tracker": "https://github.com/abdullahal-baki/sample_package/issues",
     },
-    
-    
 )
+
 
 # version >> (major, minor, patch)
     # major: Incremented for big changes, like breaking changes or significant new features.
